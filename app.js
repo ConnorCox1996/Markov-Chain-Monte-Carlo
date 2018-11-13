e6547588cf1ac68616c01b3a365fbc02c3e42bf7
ConnorCox1996 <ccox10@ur.rochester.edu>
2018-11-12 21:53:00 -0500

    changed main function to iterate through M-H algorithm 100x

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const jsnx = require('jsnetworkx');
-
-
-/* Project Notes:
-- Need to Get Travis build working
-- Need to set up system to handle CLA...
-- Need to address that I don't calculate q
-    (maybe figure a way to calculate it from randomly generated edgelist?)
-
-
-    Add descriptions to all functions...
-*/
-
 
 /*Takes as input M number of nodes to be generated
 Generates the coordinates of M number of nodes in a quasi-grid arrangement
@@ -34,6 +21,7 @@ function nodeCoords(M) {
         
 };
 
+//Generating initial node coordinates
 var coordinates = nodeCoords(4);
 
 /*Takes as input a list of node coordinates, and two nodes in that list
@@ -80,7 +68,9 @@ function nodeDistanceMatrix(coordinateList){
     return matrix;
 };
 
+//creating a matrix of all possible edge weights
 var weightMatrix = nodeDistanceMatrix(coordinates);
+//creating list of initial edges, this list contains all possible edges for a graph with 4 nodes
 var initialEdgeList = [[0, 1], [0, 3], [0, 2], [1,2], [1,3], [2,3]];
 
 /*Takes as an input a graph too add edges to & a list of edges to be added
@@ -97,6 +87,7 @@ function addEdge(edgeList){
     return edgeList;
 };
 
+//adding weight to initial graph edges 
 var initialEdgesWithWeights = addEdge(initialEdgeList);
 /*
 console.log('initial edge list with weight');
@@ -105,7 +96,7 @@ console.log(initialEdgeList);
 
 /*Takes as input a list of edges with weights
 Calculates the distance between the endpoints of the edge, and adds as a weight
-returns a modified version of the input edge list
+Returns a modified version of the input edge list
 */
 function adjacencyMatrix(edgeListWithWeights){
     var adjacency = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
@@ -124,16 +115,7 @@ function adjacencyMatrix(edgeListWithWeights){
     };     
     return adjacency;     
 };
-/*
-console.log('initial Adj Mat');
-var initialGraphAdjacencyMatrix = adjacencyMatrix(initialEdgeList);
-console.log(initialGraphAdjacencyMatrix);
-console.log('');
-*/
-/*
-var secondaryEdgeList = [[0,1],[1,2],[0,2]];
-var edgeList2WithWeights =addEdge(secondaryEdgeList);
-*/
+
 /*Takes as input a list of weighted edges and the adjacency matrix associated with that list of edges
 Determines if the graph associated with the list of egdes is connected or not
 if CONNECTED returns true
@@ -159,27 +141,24 @@ function connected(edgeList, adjMatrix){
     };   
 };
     
-/*
-console.log('HERE IS THEEEEEconnectedness checker');
-var edges = [[0,2,1],[0,1,1],[2,2,Math.sqrt(2)],[2,3,1]];
-var AM = adjacencyMatrix(edges);
-console.log(connected(edges, AM));
-*/
-
-
 /* Takes as an input upper and lower bounds for a generated number
   Returns a random integer between min (inclusive) and max (inclusive)
  */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
+//a list of al possible edges, which will be drawn from to create proposed graphs
 var listOfPossibleEdges = [[0, 1], [0, 2], [0, 3], [1, 3], [1, 2], [3, 2]];
 
+/*
+Generates a proposed graph by 
+--> randomly selecting the number of edges present, 
+for a graph with 4 nodes, connection is possible with 3, 4, 5, or 6 edges 
+so number of selected edges is a random integer between 3&6
+--> then randomly selecting the determined number of edges from a list of all possible edges
+*/
 function generateNewGraph(){
     var numEdges = getRandomInt(3,6);
-    //console.log('number of edges in graph');
-    //console.log(numEdges);
     var newEdgeList = [];
     for( var i = 0; i < 100; i++){
         var addEdge = getRandomInt(0,5);
@@ -200,20 +179,18 @@ function generateNewGraph(){
     };
     
 };
-/*
-var ex1 = generateNewGraph();
-var ex2 = generateNewGraph();
-console.log('generatedGraphs');
-console.log(ex1);
-console.log(ex1.length);
-console.log(ex2);
-console.log(ex2.length);
-*/
 
+/*Takes as an input a value to look for in an array, and the array to look for that value in
+If the value is found, returns 1
+*/
 function isInArray(value, array){
     return array.indexOf(value) > -1;
 };
 
+/*Takes as an input a list of weighted edges
+Finds sum of all shortest paths from source node 0 to every other node in the graph
+returns that sum
+*/
 function sourceTargetPath(edgelistWithWeight){
     var source = 0;
     var targetList = [10, 10, 10, 10];
@@ -241,22 +218,25 @@ function sourceTargetPath(edgelistWithWeight){
         };
     };   
 };
-console.log('Total Path Length');
-console.log(sourceTargetPath(initialEdgesWithWeights));
 
+/*Takes as an input a list of weighted edges
+Calculates the sum of all weighted edges in the graph
+Returns that sum
+*/
 function edgeWeightSum(edgeListWithWeights){
     var edgeSum = 0
-    //console.log(edgeListWithWeights);
+
     for(var i = 0; i < edgeListWithWeights.length; i++){
         edgeSum += edgeListWithWeights[i][2];
 
     };
     return edgeSum;
 };
-var sum1 = edgeWeightSum(initialEdgesWithWeights);
-console.log('Sum of all (weighted Edges)');
-console.log(sum1);
 
+/*Takes as an input a list of edges (doesn't need to be weighted)
+Calculates theta for that list of edges
+Returns the theta value
+*/
 function theta(edgeList){
     var sum1 = sourceTargetPath(edgeList);
     var r =1;
@@ -265,15 +245,15 @@ function theta(edgeList){
     return result;
 };
 
-var mocklist = [[0,1,1],[0,2,1],[1,3,1],[2,3,1],[0,3, Math.sqrt(2)]];
-var mockresult = theta(mocklist);
-console.log('theta');
-console.log(mockresult)
-
-
-
+/* a list of factorials 0 through 6
+this list of factorials is used to calculate q for given states
+*/
 var factorials = [0, 1, 2, 6, 24, 120, 720];
 
+/*Takes as an input a list of edges
+calculates q, the probability of the state denoted by its edgelist being generated
+Returns that q value
+*/
 function q(edgeList){
     var edgesPresent = edgeList.length;
     var edgesPossible = 6;
@@ -288,6 +268,10 @@ function q(edgeList){
     return q
 };
 
+/*Takes as an input a current state(graphOne) & a proposed state(graphTwo)
+Calculates Pij/Pii according to the formula given
+Returns that ratio
+*/
 function piJI(graphOne, graphTwo){
     var T = 1;
     var diff = theta(graphOne) - theta(graphTwo);
@@ -296,19 +280,9 @@ function piJI(graphOne, graphTwo){
     return resultpi;
 };
 
-var graphOne = [[0,1,1],[0,2,1],[,3,1][2,3,1]];
-var graphTwo = [[0,1,1],[0,2,1],[1,3,1],[2,3,1],[0,3, Math.sqrt(2)]];
-
-var mocklist = [[0,1,1],[0,2,1], [1,3,1]];
-console.log(theta(mocklist));
-var mocklist2 = [[0,1,1],[0,2,1],[1,3,1],[2,3, 1]];
-console.log(theta(mocklist2));
-
-var tester = piJI(mocklist, mocklist2);
-console.log('pi result')
-console.log(tester);
-
-/*takes as input two graphs as an ordered pair, where current graph is first & proposed graph is second
+/*Takes as input two graphs as an ordered pair, where current graph is first & proposed graph is second
+Uses the Metropolis-Hastings Algorithm to determine whether to accept or reject the proposed state
+Returns true if the state is accepted, false if the state is rejected
 */
 function acceptOrReject(graphOne, graphTwo){
     var pi = piJI(graphTwo, graphOne);
@@ -318,82 +292,79 @@ function acceptOrReject(graphOne, graphTwo){
     var proposal = pi * qRatio;
 
     if(proposal >= 1){
-        console.log('here 1')
         return [1, true, 'A =>1'];
     } else{
         var u = getRandomInt(0,1);
         if(u <= proposal){
-            console.log('here 2')
             return [2, true, 'A < 1, but proposal => u'];
         } else{
-            console.log('here 3')
             return [3, false, 'A <1 & A < u']
         };
     };
 };
-console.log('accept');
-console.log(acceptOrReject(mocklist, mocklist2));
 
+/*Takes as an input the number of iterations which are desired
+NOTE more than 15,000 iterations is expected to take over a minute to process
+Iterates through the Metropolis-Hastings Algorithm
+Returns a list of adjacency matrices for all accepted states
+*/
 function main(iterations){
     var hold = [[],[]];
+    var store = [];
     var save = [];
     var X0 = initialEdgesWithWeights;
     var thetaX0 = theta(initialEdgesWithWeights);
-    console.log('THETA 0');
-    console.log(thetaX0);
     hold[0][0] = X0;
     var qX0 = q(initialEdgesWithWeights);
     hold[0][1] = qX0;
 
     for(var i =0; i < iterations; i++){
+        var storeEdges = []
         var proposal = generateNewGraph();
         var weightedEdgeListi = addEdge(proposal);
         var adjacMatrix = adjacencyMatrix(proposal);
         var connection = connected(weightedEdgeListi,adjacMatrix);
         if(connection == true){
-            console.log('EDGES success');
-            console.log(weightedEdgeListi);
             var thetai = theta(weightedEdgeListi);
-            console.log('THETA 1');
-            console.log(thetai);
             hold[1][0] = proposal;
             var qXi = q(weightedEdgeListi);
             hold[1][1] = qXi;
             var acc = acceptOrReject(hold[0][0], hold[1][0]);
-            console.log('ACC')
-            console.log(acc);
+
                 if(acc[1] == true){
                     hold[0][0] = proposal;
+                    console.log('proposal' + ' ' + i + ' ' + 'accepted: ');
+                    console.log('');
+                    storeEdges[i] = proposal.length;
                     var output = adjacencyMatrix(proposal);
+                    console.log(output);
                     for(var k=0; k < output.length; k++){
                         for(var l =0; l < output.length; l++){
                             if(output[k][l] != 0){
-                                output[k][l] = 1;
+                                save.push(1);
 
+                            }else{
+                                save.push(0);
                             }
                         }
-                    }
-                    save[i] = output;
+                    }   
+                var save1 =save.join('');
+                var save2 = parseInt(save1,2);
+        store[i]=save2
                         
                 }else{
-                console.log('EDGES fail connection');
-                console.log(weightedEdgeListi);
+                    console.log('proposal ' + i + ' rejected');
+                    storeEdges[i] = hold[0][0].length;
+
             };
-        };          
+        };
+
     };
-return save;
+    return;
 };
 
-console.log('README');
-console.log('the big finish');
-console.log(main(2));
-
-
-
-
-
-
-
+var run = main(100);
+console.log(run);
 
 
 
